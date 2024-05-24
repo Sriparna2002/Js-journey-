70ffceff6a9c10912e8c39ce4a565e6ccbf28006
Sriparna2002 <sriparna.majumder2002@gmail.com>
2024-05-24 21:39:55 +0530

    Day 3

---
+++ day3(Data types).js
@@ -0,0 +1,21 @@
+"use strict" ; // treate all js code as newer version 
+
+// alatrt (3+3) // we are using nodejs, not browser
+
+let name = "Hellow World"; // string
+let age = 18; // number
+let isloggedIn = false; // boolean
+let state = null; // type : object 
+
+// primitive datatype
+// number ==> 2 to power 53
+// bigint
+// string ==>""
+// boolean ==> true/false
+// null ==> standalone value 
+// undefined (type : undefined)
+// symbol ==> unique
+
+
+console.log(typeof null);
+console.log(typeof undefined);
