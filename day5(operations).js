709c9fee38ebaa3f07fd20b1850e13a857209475
Sriparna2002 <sriparna.majumder2002@gmail.com>
2024-05-24 22:38:16 +0530

    Day 5

---
+++ day5(operations).js
@@ -0,0 +1,37 @@
+//====== operations =======//
+
+let value = 3 
+let negValue = -value
+console.log(negValue);
+
+//basic math operations 
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/2);
+console.log(2%3);
+
+
+concatination 
+let str1 = "hello"
+let str2 = "world"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(true);
+console.log(+"");
+
+let num1, num2, num3
+num4 = num1 = num2 = num3 = 2+2
+console.log(num4);
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter);
